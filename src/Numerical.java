00cbf0d95d81a16a02c0392fc6b840842ab1916e
hannahlamas <81644186+hannahlamas@users.noreply.github.com>
2025-01-31 18:38:42 +0000

    fourth commit

---
+++ src/Numerical.java
@@ -0,0 +1,25 @@
+public class Numerical{
+    public static void main(String[] args){
+
+// Convert the string strFloat into a float.
+String strFloat = "374.25";
+float from_strFloat; // Float.parseFloat("374.25")
+
+// Convert the string strInt into an integer.
+String strInt = "1250";
+int from_strInt; // Integer.parseInt("1250")
+
+// Convert the float floatNum into an integer.
+float floatNum = 56.715f;
+int from_floatNum; // (int) 56.715f;
+
+// Convert the integer intNum into a float.
+int intNum = 1500;
+float from_intNum; // (float) 5;
+
+// Convert the double doubleNum into a string.
+double doubleNum = 3.14159265;
+String from_doubleNum; // Double.toString(3.14159265);
+
+    }
+}
